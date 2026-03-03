574fc3683e49920073dc755d1da29b24c5005e4f
ldbonequi <ldbonequi@gmail.com>
2026-03-03 15:33:50 -0700

    add comments, create main

--- satSolver.py
+++ satSolver.py
@@ -2,41 +2,59 @@ class literal:
     def __init__(self, identity) -> None:
         self.identity: str = identity
 
-    def eval(self, map):
+    def eval(self, literal_values: list[bool]):
+        """
+        returns the value of the literal
+        params:
+            literal_values: list of literal_values (xn) at index n
+        """
+        # return opposite of value if value has not
         if "~" in self.identity:
-            val = map[self.identity[1]]
-            return 0 if val == 1 else 1
+            val = literal_values[int(self.identity[2])]
+            return False if val else True
         else:
-            return map[self.identity]
+            return literal_values[int(self.identity[1])]
 
-    def __str__(self) -> str:
+    def __str__(self) -> str:  # enable printing literals
         return self.identity
 
-    def __repr__(self) -> str:
+    def __repr__(self) -> str:  # properly display literal inside of printed lists
         return self.identity
 
 
 def parseInput(input):
     terms: list[list[literal]] = list()
     term: list[literal] = list()
-    negative = False
+    negative: bool = False
+    literals: set = set()
 
     for c in input:
-        if c == "." or c == ")":
+        if c == "." or c == ")":  # and, start new term
             if term:
                 terms.append(term)
                 term = list()
-        elif c == "~":
+        elif c == "~":  # next number is negative
             negative = True
-        elif c.isdigit():
+        elif c.isdigit():  # found literal
             if negative:
-                term.append(literal("~" + c))
+                term.append(literal("~x" + c))
                 negative = False
             else:
-                term.append(literal(c))
+                term.append(literal("x" + c))
+            literals.add(c)
+
     if term:
         terms.append(term)
-    return terms
+    return len(literals), terms
 
 
-print(parseInput("(~x1+ x2) (x3 + ~x4) x1"))
+def main():
+    size: int
+    terms: list[list[literal]]
+    size, terms = parseInput("(~x1+ x2) (x3 + ~x4) x1")
+    print(f"terms: {terms}")
+    print(f"size {size}")
+
+
+if __name__ == "__main__":
+    main()
