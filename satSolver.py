ae4e42a0b79a9e9c87558fec5434b8180182de57
ldbonequi <ldbonequi@gmail.com>
2026-03-03 17:57:27 -0700

    move expression into class and fix logic error in not literals

--- satSolver.py
+++ satSolver.py
@@ -5,18 +5,23 @@ class literal:
     def __init__(self, identity: str) -> None:
         self.identity: str = identity
 
-    def eval(self, literal_values: list[bool]):
+    def eval(self, literal_values: list[bool | None]):
         """
-        returns the value of the literal
+        returns the value of the literal as True,False,None
         params:
-            literal_values: list of literal_values (xn) at index n
+            literal_values: list of literal_values (xn) at index n-1
         """
         # return opposite of value if value has not
         if "~" in self.identity:
-            val = literal_values[int(self.identity[2])]
-            return False if val else True
+            val = literal_values[int(self.identity[2]) - 1]
+            if val:
+                return False
+            elif val is None:
+                return None
+            elif not val:
+                return True
         else:
-            return literal_values[int(self.identity[1])]
+            return literal_values[int(self.identity[1]) - 1]
 
     def __str__(self) -> str:  # enable printing literals
         return self.identity
@@ -25,33 +30,69 @@ class literal:
         return self.identity
 
 
-def parseInput(input: str):
-    """
-    takes a cnf expression and returns a count of the literals and a list of clauses
-    params:
-        input: cnf expression as string
-    """
-    clauses: list[list[literal]] = list()
-    literals: set = set()
+class expression:
+    def __init__(self, expression: str) -> None:
+        self.expression: str = expression
+        self.clauses: list[list[literal]] = list()
+        self.literal_count: int = int()
+        self.__parseInput__(expression)
+        self.literal_values: list[bool | None] = [None] * self.literal_count
 
-    input = input.strip(" +(")  # remove useless chars
-    clause_strs = [c for c in re.split(r"[.)]", input) if c]  # split clauses
-    for c in clause_strs:
-        clause = list()
-        for item in re.findall(r"~?x\d+", c):  # find all literals
-            clause.append(literal(item))
-            literals.add(item.strip("~"))
-        clauses.append(clause)
+    def __parseInput__(self, input: str):
+        """
+        takes a cnf expression and populates sefl.literal_count and self.clauses
+        params:
+            input: cnf expression as string
+        """
+        literals: set = set()
+        input = input.strip(" +(")  # remove useless chars
+        clause_strs = [c for c in re.split(r"[.)]", input) if c]  # split clauses
+        for c in clause_strs:
+            clause = list()
+            for item in re.findall(r"~?x\d+", c):  # find all literals
+                clause.append(literal(item))
+                literals.add(item.strip("~"))
+            self.clauses.append(clause)
+        self.literal_count = len(literals)
 
-    return len(literals), clauses
+    def __str__(self) -> str:
+        return self.expression
+
+    def sat(self):
+        pass  # TODO create basic exhaustive search
+
+    def assign(self, literal: str, value: bool | None):
+        idx = int(literal.strip("x")) - 1
+        self.literal_values[idx] = value
+
+    def clause_eval(self, clause: list[literal]):
+        """
+        returns True if the clause is satisfied
+        returns False if the clause is unsat
+        returns None if the clause is undetermined
+        params:
+            clause: clause to evaluate
+        """
+        to_return = False
+        for lit in clause:
+            lit = lit.eval(self.literal_values)
+            if lit is None and not to_return:
+                to_return = None
+            if lit:
+                return True
+        return to_return
+
+    def print_literal_values(self):
+        for i in range(1, self.literal_count + 1):
+            print(f"x{i}: {self.literal_values[i - 1]}")
 
 
 def main():
-    literal_count: int
-    clauses: list[list[literal]]
-    literal_count, clauses = parseInput("(~x1 + x2).(x3 + ~x4) x1")
-    print(f"terms: {clauses}")
-    print(f"literal count: {literal_count}")
+    exp = expression("(~x1 + x2).(x3 + ~x4) x1")
+    exp.assign("x1", True)
+    exp.assign("x2", True)
+    exp.print_literal_values()
+    print([exp.clause_eval(clause) for clause in exp.clauses])
 
 
 if __name__ == "__main__":
